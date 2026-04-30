e23367537146d430c458a51adc49d7390ecd7814
Matheus Barbosa <matheus.barbosa@sankhya.com.br>
2026-04-30 15:00:59 -0300

    correção mtractor

---
+++ src/br/com/sankhya/bhz/central/acoes/acaoDefinePrioridade.java
@@ -0,0 +1,38 @@
+package br.com.sankhya.bhz.central.acoes;
+
+import br.com.sankhya.extensions.actionbutton.AcaoRotinaJava;
+import br.com.sankhya.extensions.actionbutton.ContextoAcao;
+import br.com.sankhya.extensions.actionbutton.Registro;
+import br.com.sankhya.jape.vo.DynamicVO;
+import br.com.sankhya.jape.wrapper.JapeFactory;
+import br.com.sankhya.jape.wrapper.JapeWrapper;
+import br.com.sankhya.modelcore.util.DynamicEntityNames;
+
+import java.math.BigDecimal;
+
+public class acaoDefinePrioridade implements AcaoRotinaJava {
+    JapeWrapper cabDAO = JapeFactory.dao(DynamicEntityNames.CABECALHO_NOTA);
+
+    @Override
+    public void doAction(ContextoAcao contexto) throws Exception {
+        Registro[] linhas = contexto.getLinhas();
+
+        if (linhas.length <= 0) {
+            contexto.mostraErro("Selecione pelo menos uma linha para atualização.");
+        }
+
+        BigDecimal nuNota = BigDecimal.ZERO;
+
+        for(Registro linha : linhas) {
+            nuNota = (BigDecimal) linha.getCampo("NUNOTA");
+
+            DynamicVO cabVO = cabDAO.findByPK(nuNota);
+
+            cabDAO.prepareToUpdate(cabVO)
+                    .set("AD_EPRIORIDADE", "S")
+                    .update();
+        }
+
+        contexto.setMensagemRetorno("Pedidos definido como prioridade.");
+    }
+}
---
+++ src/br/com/sankhya/bhz/central/acoes/acaoRemovePrioridade.java
@@ -0,0 +1,38 @@
+package br.com.sankhya.bhz.central.acoes;
+
+import br.com.sankhya.extensions.actionbutton.AcaoRotinaJava;
+import br.com.sankhya.extensions.actionbutton.ContextoAcao;
+import br.com.sankhya.extensions.actionbutton.Registro;
+import br.com.sankhya.jape.vo.DynamicVO;
+import br.com.sankhya.jape.wrapper.JapeFactory;
+import br.com.sankhya.jape.wrapper.JapeWrapper;
+import br.com.sankhya.modelcore.util.DynamicEntityNames;
+
+import java.math.BigDecimal;
+
+public class acaoRemovePrioridade implements AcaoRotinaJava {
+    JapeWrapper cabDAO = JapeFactory.dao(DynamicEntityNames.CABECALHO_NOTA);
+
+    @Override
+    public void doAction(ContextoAcao contexto) throws Exception {
+        Registro[] linhas = contexto.getLinhas();
+
+        if (linhas.length <= 0) {
+            contexto.mostraErro("Selecione pelo menos uma linha para atualização.");
+        }
+
+        BigDecimal nuNota = BigDecimal.ZERO;
+
+        for(Registro linha : linhas) {
+            nuNota = (BigDecimal) linha.getCampo("NUNOTA");
+
+            DynamicVO cabVO = cabDAO.findByPK(nuNota);
+
+            cabDAO.prepareToUpdate(cabVO)
+                    .set("AD_EPRIORIDADE", "N")
+                    .update();
+        }
+
+        contexto.setMensagemRetorno("Pedidos prioridade removida com sucesso.");
+    }
+}
